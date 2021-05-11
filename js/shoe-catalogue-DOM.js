9f445c98179f1b16fceadb62ad18e0cb2130660f
clarababette <clara_coetzee@zoho.com>
2021-05-11 10:29:00 +0200

    css check

---
+++ js/shoe-catalogue-DOM.js
@@ -0,0 +1,59 @@
+//dom add shoe-icon class and set viewbox
+
+Handlebars.registerHelper("stockCheck", (size) => {
+  if (size === undefined || size === 0) {
+    return "out-of-stock";
+  } else {
+    return "in-stock";
+  }
+});
+
+Handlebars.registerHelper("viewBox", (brand) => {
+  switch (brand) {
+    case "Crocs":
+      return new Handlebars.SafeString("'-5 0 522 380'");
+    case "Converse":
+      return new Handlebars.SafeString("'-5 10 522 400'");
+    case "Vans":
+      return new Handlebars.SafeString("'-5 0 522 380'");
+    case "Dr Martens":
+      return new Handlebars.SafeString("'0 0 522 512'");
+    case "Nike":
+      return new Handlebars.SafeString("'-5 0 522 380'");
+  }
+});
+
+Handlebars.registerHelper("icon", (brand) => {
+  switch (brand) {
+    case "Crocs":
+      return new Handlebars.SafeString("#crocs");
+    case "Converse":
+      return new Handlebars.SafeString("#converse");
+    case "Vans":
+      return new Handlebars.SafeString("#vans");
+    case "Dr Martens":
+      return new Handlebars.SafeString("#dr-martens");
+    case "Nike":
+      return new Handlebars.SafeString("#nike");
+  }
+});
+
+Handlebars.registerHelper("displayColor", (color) => {
+  let thisColor = color.split("-");
+  return thisColor[0];
+});
+
+theseShoes = shoeData["shoe"];
+theseShoes.forEach((thisShoe) => {
+  rule = "." + thisShoe.colorname + "{ --mid: " + thisShoe.color + "}";
+  var styles = document.CSS;
+  styles.insertRule(rule);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var templateSource = document.querySelector("#testTemplate").innerHTML;
+  var testTemplate = Handlebars.compile(templateSource);
+
+  var shoeStock = document.querySelector(".shoe-stock");
+  shoeStock.innerHTML = testTemplate(shoeData);
+});
